4cc9933bf899c56db99f3a9caa3d60b23213d827
Helena <Helena.adi@live.fr>
2020-01-31 16:02:22 +0100

    semainiers

---
+++ Semainier_nutri.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<?php include("entete.php"); ?>
+
+<h2>Semainier nutritionnel</h2>
+
+<body>
+    <table border=1 cellspacing=4 cellpadding=4 class="semainier">
+        <tr>
+            <th>Repas/Jour</th>
+            <th>Lundi</th>
+            <th>Mardi</th>
+            <th>Mercredi</th>
+            <th>Jeudi</th>
+            <th>Vendredi</th>
+            <th>Samedi</th>
+            <th>Dimanche</th>
+        </tr>
+        <tr>
+            <td>Petit-déjeuner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Collation</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Déjeuner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Collation</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Dîner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+    </table>
+
+</body>
+
+
+
+</html>
---
+++ Semainier_ress.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<?php include("entete.php"); ?>
+
+<h2>Semainier nutritionnel</h2>
+
+<body>
+    <table border=1 cellspacing=4 cellpadding=4 class="semainier">
+        <tr>
+            <th>Repas/Jour</th>
+            <th>Lundi</th>
+            <th>Mardi</th>
+            <th>Mercredi</th>
+            <th>Jeudi</th>
+            <th>Vendredi</th>
+            <th>Samedi</th>
+            <th>Dimanche</th>
+        </tr>
+        <tr>
+            <td>Petit-déjeuner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Collation</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Déjeuner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Collation</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Dîner</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+            <td>Croissant</td>
+        </tr>
+        <tr>
+            <td>Ressenti</td>
+            <td><input type="text" style="height:100px;" name="Ress_lundi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_mardi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_mercredi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_jeudi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_vendredi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_samedi"/>
+            <input type="button" value="Ok"></td>
+            <td><input type="text" style="height:100px;" name="Ress_dimanche"/>
+            <input type="button" value="Ok"></td>
+        </tr>
+    </table>
+
+</body>
+
+
+
+</html>
