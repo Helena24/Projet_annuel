239e7c695a1010eb322fd1b312cb38058aabaa52
Victor Janneteau <victor.janneteau@laposte.net>
2020-02-04 15:19:47 +0100

    add

--- Add.form.php
+++ Add.form.php
@@ -33,8 +33,27 @@ catch(PDOException $e)
 // Ajout des champs dans la table Clients 
 
 
+// if(isset($_POST['add']))
+// {		
+//     $nomUser=$_POST['nomUser'];
+//     $prenomUser=$_POST['prenomUser'];
+//     $datenaissanceUser=$_POST['datenaissanceUser'];
+//     $emailUser=$_POST['emailUser'];
+//     $telUser=$_POST['telUser'];
+//     $adresseUser=$_POST['adresseUser'];
+
+// 		$Requete = $bdd->prepare("INSERT INTO CLIENTS (NOM_CLIENT,PRENOM_CLIENT,DATE_NAISSANCE_CLIENT,MAIL_CLIENT,TEL_CLIENT,ADRESSE_POSTALE_CLIENT) VALUES(?,?,?,?,?,?)");
+// 		$Requete->bindValue(1,$nomUser, PDO::PARAM_STR);
+// 		$Requete->bindValue(2,$prenomUser, PDO::PARAM_STR);
+// 		$Requete->bindValue(3,$datenaissanceUser, PDO::PARAM_INT);
+// 		$Requete->bindValue(4,$emailUser, PDO::PARAM_STR);
+// 		$Requete->bindValue(5,$telUser, PDO::PARAM_STR);
+// 		$Requete->bindValue(6,$adresseUser, PDO::PARAM_STR);
+// 		$Requete->execute();
+// }
+
 if(isset($_POST['add']))
-{		
+{
     $nomUser=$_POST['nomUser'];
     $prenomUser=$_POST['prenomUser'];
     $datenaissanceUser=$_POST['datenaissanceUser'];
@@ -42,15 +61,20 @@ if(isset($_POST['add']))
     $telUser=$_POST['telUser'];
     $adresseUser=$_POST['adresseUser'];
 
-		$Requete = $bdd->prepare("INSERT INTO CLIENTS (NOM_CLIENT,PRENOM_CLIENT,DATE_NAISSANCE_CLIENT,MAIL_CLIENT,TEL_CLIENT,ADRESSE_POSTALE_CLIENT) VALUES(?,?,?,?,?,?)");
-		$Requete->bindValue(1,$nomUser, PDO::PARAM_STR);
-		$Requete->bindValue(2,$prenomUser, PDO::PARAM_STR);
-		$Requete->bindValue(3,$datenaissanceUser, PDO::PARAM_INT);
-		$Requete->bindValue(4,$emailUser, PDO::PARAM_STR);
-		$Requete->bindValue(5,$telUser, PDO::PARAM_STR);
-		$Requete->bindValue(6,$adresseUser, PDO::PARAM_STR);
-		$Requete->execute();
+	$Requete = $bdd->prepare("INSERT INTO CLIENTS (NOM_CLIENT,PRENOM_CLIENT,DATE_NAISSANCE_CLIENT,MAIL_CLIENT,TEL_CLIENT,ADRESSE_POSTALE_CLIENT) 
+    VALUES(:nomUser, :prenomUser, :datenaissanceUser, :emailUser, :telUser, :adresseUser)");
+	$Requete->bindValue(":nomUser",$nomUser, PDO::PARAM_STR);
+	$Requete->bindValue(":prenomUser",$prenomUser, PDO::PARAM_STR);
+    $Requete->bindValue(":datenaissanceUser",$datenaissanceUser, PDO::PARAM_STR);
+    $Requete->bindValue(":emailUser",$emailUser, PDO::PARAM_STR);
+    $Requete->bindValue(":telUser",$telUser, PDO::PARAM_STR);
+    $Requete->bindValue(":adresseUser",$adresseUser, PDO::PARAM_STR);
+	$Requete->execute();
 }
+
+
+
+
 ?>
 
 </body> 
