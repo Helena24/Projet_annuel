e28f8c49be05b59377ab9c84787dcce949bd926c
Helena <Helena.adi@live.fr>
2020-02-06 16:02:26 +0100

    affiche aliment suppr

---
+++ suppr_aliment.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<?php include("entete.php"); ?>
+<?php include("police.php"); ?>
+<?php include("Connect.php"); ?>
+<head>
+<title>Suppression Aliment</title>
+</head> 
+<body>
+
+<table border=1 cellspacing=4 cellpadding=4 class="semainier">
+        <tr>
+            <th>Nom</th>
+            <th>Catégorie</th>
+            <th>Calories</th>
+            <th>Protéines</th>
+            <th>Glucides</th>
+            <th>Lipides</th>
+            <th>Supprimer</th>
+        </tr>
+
+<?php
+$reponse = $connect->query('SELECT * FROM ALIMENTS');
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+?>
+    <tr>
+    <td><?php echo $donnees['NOM_ALIMENT']; ?></td>
+    <td><?php echo $donnees['CATEGORIE_ALIMENT']; ?></td>
+    <td><?php echo $donnees['CALORIE_ALIMENT']; ?></td>
+    <td><?php echo $donnees['PROTEINE_ALIMENT']; ?></td>
+    <td><?php echo $donnees['GLUCIDE_ALIMENT']; ?></td>
+    <td><?php echo $donnees['LIPIDE_ALIMENT']; ?></td>
+    <td><input name="supp" type="submit" value="Supprimer"/></td>
+    </tr>
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
+</body>
+
+
+
+
+
+
+</html>
