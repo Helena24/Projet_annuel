a313d800276b025ffb2bdb1bb27677cb8d48389a
Desbourdes Lucas <lucasdesbourdes@gmail.com>
2020-02-14 14:40:07 +0100

    connect

--- Add.mensurations.php
+++ Add.mensurations.php
@@ -2,7 +2,6 @@
 <html>
 <?php include("entete.php"); ?>
 <?php include("police.php"); ?>
-<?php include("Connect.php"); ?>
     <head>
         <title>Mensurations</title>
     </head>
--- Mesures.php
+++ Mesures.php
@@ -9,20 +9,20 @@
 
 <center>
 <h3>Veuillez remplir attentivement chacun des champs ci dessous</h3>
-<form method="POST" action= "Add.mesures.php" enctype="multipart/form-data"></form>
+<form method="POST" action= "Add.mesures.php">
 <fieldset>
-    Date :                      <input name="DateB" type="date" required ><br>
+    Date :                                         <input name="DateB" type="date" required ><br>
     Pourcentage de masse graisseuse :              <input name="PourcentMG" required ><br>
-    Poids (en kg) :                <input name="Masse" required ><br>
-    Pourcentage d'eau dans le corps :       <input name="PourcentH2O" required ><br>
-    Graisse viscérale :         <input name="GV" required ><br>
-    Masse musculaire :     <input name="Massemuscu" required ><br>
-    Indice d'effort physique :      <input name="Indiceeffort" required ><br>
-    Masse osseuse :      <input name="Masseosseuse" required ><br>
-    Impédance du corps :       <input name="Impedance" required ><br>
+    Poids (en kg) :                                <input name="Masse" required ><br>
+    Pourcentage d'eau dans le corps :              <input name="PourcentH2O" required ><br>
+    Graisse viscérale :                            <input name="GV" required ><br>
+    Masse musculaire :                             <input name="Massemuscu" required ><br>
+    Indice d'effort physique :                     <input name="Indiceeffort" required ><br>
+    Masse osseuse :                                <input name="Masseosseuse" required ><br>
+    Impédance du corps :                           <input name="Impedance" required ><br>
     
 
-    <br> <input name="add" type="submit" value="Envoyer au Nutritionniste"/>
+    <br> <input type="submit"  name="envoyer"  value="Envoyer au Nutritionniste"/>
 </fieldset>
 </form>
 </center>
