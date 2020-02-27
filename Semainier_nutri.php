72510a5266d1a652ad528ca2b6cb48283f90399a
Helena <Helena.adi@live.fr>
2020-02-27 16:37:37 +0100

    semainier_nutri_client_date

--- Semainier_nutri.php
+++ Semainier_nutri.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <html>
 
-<?php include("entete.php"); ?>
+<?php include("entete.php"); 
+include("Connect.php");?>
 
 <h2>Semainier nutritionnel</h2>
+
+
+<br>
+<label for="client">Client : </label>
+<select name="Client">
+<?php
+$reponse = $connect->query('SELECT NOM_CLIENT FROM CLIENTS');
+while ($donnees = $reponse->fetch())
+{
+echo '<option value="' . $donnees['NOM_CLIENT'] . '">' . $donnees['NOM_CLIENT'] . '</option>';
+}                      
+?>
+</select>
+Date de début : <input name="datedebutSem" type="date" required >
+Date de fin : <input name="datefinSem" type="date" required >
+<br><br>
+
 
 <body>
     <table border=1 cellspacing=4 cellpadding=4 class="semainier">
