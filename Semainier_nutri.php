c15e82b57b8473e8fa4d13cf1ff258546943c6f1
Helena <Helena.adi@live.fr>
2020-03-13 11:05:42 +0100

    gf

--- Semainier_nutri.php
+++ Semainier_nutri.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<script type="text/javascript" src="semainier.js"></script>
 
 <?php include("entete.php"); 
 include("Connect.php");?>
@@ -22,7 +23,7 @@ Date de début : <input name="datedebutSem" type="date" required >
 Date de fin : <input name="datefinSem" type="date" required >
 <br><br>
 
-
+<form method="POST" action= "Semainier_nutri_ajout.php" enctype="multipart/form-data">
 <body>
     <table border=1 cellspacing=4 cellpadding=4 class="semainier">
         <tr>
@@ -37,7 +38,46 @@ Date de fin : <input name="datefinSem" type="date" required >
         </tr>
         <tr>
             <td>Petit-déjeuner</td>
-            <td>Croissant</td>
+            <td>Aliment :
+            <select name="Aliment" id="Aliment">
+            <?php
+            $aliments = $connect->query('SELECT NOM_ALIMENT FROM ALIMENTS');
+            while ($donnees = $aliments->fetch())
+            {
+            echo '<option value="' . $donnees['NOM_ALIMENT'] . '">' . $donnees['NOM_ALIMENT'] . '</option>';
+            }                      
+            ?>
+            </select>
+            <input name="Enregistrer" type="submit" value="+"/>
+            <script>
+                var selectElmt = document.getElementById("Aliment");
+                var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
+                document.write(valeurselectionnee);
+            </script>
+            <br>
+            Recette :
+            <select name="Recette">
+            <?php
+            $recettes = $connect->query('SELECT NOM_RECETTE FROM RECETTES');
+            while ($donnees = $recettes->fetch())
+            {
+            echo '<option value="' . $donnees['NOM_RECETTE'] . '">' . $donnees['NOM_RECETTE'] . '</option>';
+            }                      
+            ?>
+            </select>
+            <input type="button" value="Ok">
+            <br>
+            Smoothie :
+            <select name="Smoothie">
+            <?php
+            $smoothies = $connect->query('SELECT NOM_SMOOTHIE FROM SMOOTHIES');
+            while ($donnees = $smoothies->fetch())
+            {
+            echo '<option value="' . $donnees['NOM_SMOOTHIE'] . '">' . $donnees['NOM_SMOOTHIE'] . '</option>';
+            }                      
+            ?>
+            </select>
+            <input type="button" value="Ok"></td>
             <td>Croissant</td>
             <td>Croissant</td>
             <td>Croissant</td>
@@ -89,6 +129,6 @@ Date de fin : <input name="datefinSem" type="date" required >
 
 </body>
 
-
+</form>
 
 </html>
