9363bdc10c3db1e81feb6c918d57677e8e988eb2
Luluimg <lucasdesbourdes@gmail.com>
2020-03-13 12:20:49 +0100

    Div alignement

--- Mensurations.php
+++ Mensurations.php
@@ -11,9 +11,12 @@
 
 <center>
 <h3>Veuillez remplir attentivement chacun des champs ci dessous</h3>
+</center>
+
+<div style="display :flex; flex-direction:wrap; padding-left: 100px">
 
 <form method="POST" action= "Add.mensurations.php">
-<fieldset>
+
     <label for="DateM">Date :</label><br>
     <input type=date name="DateM" required /><br>
     <label for="Taille">Taille (en cm) :</label><br>
@@ -38,8 +41,166 @@
     <input name="mollet" required /><br>
 
     <br> <input type="submit" name="enregistrer" value="Enregistrer"/>
-</fieldset>
+
 </form>
-</center>
+
+
+<div style="width:150px; padding-left:250px;">
+<table border=1 cellspacing=4 cellpadding=4 class="semainier">
+        <tr>
+            <th>Date</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['DATE_MENSURATION']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Taille</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TAILLE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Poids</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['POIDS_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour d'épaules</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_EPAULE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de poitrine</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_POITRINE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de bras</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_BRAS_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de poignet</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_POIGNET_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de taille</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_TAILLE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de hanche</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_HANCHE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de cuisse</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_CUISSE_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Tour de mollets</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MENSURATIONS');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['TOUR_MOLLET_CLIENT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+    
 </body>
 </html>
--- Mesures.php
+++ Mesures.php
@@ -9,8 +9,11 @@
 
 <center>
 <h3>Veuillez remplir attentivement chacun des champs ci dessous</h3>
+</center>
+
+<div style="display :flex; flex-direction:wrap; padding-left: 100px">
 <form method="POST" action= "Add.mesures.php">
-<fieldset>
+
     <label for="DateB">Date :</label><br>
     <input type="date" name="DateB" required /><br>
     <label for="PourcentMG">Pourcentage de masse graisseuse :</label><br>
@@ -31,8 +34,137 @@
     <input name="Agemetabolique" required /><br>
 
     <br> <input type="submit"  name="enregistrer"  value="Enregistrer"/>
-</fieldset>
+
 </form>
-</center>
+
+
+<div style="width:150px; padding-left:250px;">
+<table border=1 cellspacing=4 cellpadding=4 class="semainier">
+        <tr>
+            <th>Date</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['DATE_MESURES']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Pourcentage masse graisseuse</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['POURCENTAGE_MASSE_GRAISSEUSE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Masse</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['MASSE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Pourcentage eau corps</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['POURCENTAGE_EAU_CORPS']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Graisse viscérale</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['GRAISSE_VISCERALE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Masse musculaire</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['MASSE_MUSCULAIRE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Indice effort</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['INDICE_EFFORT']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Masse osseuse</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['MASSE_OSSEUSE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
+        <tr>
+            <th>Age métabolique</th>
+            <?php
+            $reponse = $connect->query('SELECT * FROM MESURES');
+            // On affiche chaque entrée une à une
+            while ($donnees = $reponse->fetch())
+            {
+            ?>
+            <td><?php echo $donnees['AGE_METABOLIQUE']; ?></td>
+            <?php
+            }
+            $reponse->closeCursor(); // Termine le traitement de la requête
+            ?>
+        </tr>
 </body>
 </html>
