0b4efa5fa0c8e3935e8aea33fecf8dbb87ae2941
Victor Janneteau <victor.janneteau@laposte.net>
2020-03-27 14:20:00 +0100

    changes

--- Add.recette.php
+++ Add.recette.php
@@ -1,64 +1,33 @@
 <?php include("Connect.php"); ?>
 <?php
-   /*  $term = $_GET['term'];
-     
-    $requete = $connect->prepare('SELECT * FROM ALIMENTS WHERE NOM_ALIMENT LIKE :term');
-    $requete->execute(array('term' => '%' .$term. '%'));
-     
-    $array = array();
-     
-    while($donnee = $requete->fetch())
-    {
-        array_push($array, $donnee['NOM_ALIMENT']);
-    }
-    echo json_encode($array);  */
-
-
-    /* $sql = $connect->prepare("SELECT * FROM ALIMENTS WHERE NOM_ALIMENT LIKE ?");
-	$sql->bind_param("s",$search_param);			
-	$sql->execute();
-	$result = $sql->get_result();
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-		$countryResult[] = $row["NOM_ALIMENT"];
-		}
-		echo json_encode($countryResult);
-	}
-    $conn->close(); */
-    
-
-
-/* 
-$q = $_GET["NOM_ALIMENT"];
-
-if (!$q) return;
-
-$query = mysql_query("SELECT NOM_ALIMENT FROM ALIMENTS WHERE NOM_ALIMENT LIKE '$q%' LIMIT 10");
-echo "blabla";
-while($row = mysql_fetch_array($query)) {
-
-    $NOM_ALIMENT = $row['NOM_ALIMENT'];
-
-   
-
-    echo "$NOM_ALIMENT\n";
-
-} */
-
-if (isset($_GET['term']))
-{
-    $secure = htmlspecialchars($_GET['term']);
-    $result = $connect->query('SELECT * FROM ALIMENTS WHERE NOM_ALIMENT LIKE "%'.$secure.'%" ORDER BY NOM_ALIMENT LIMIT 25');
+  
+    $result = $connect->query('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
     $result = $result->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($result);
-}
+    $res = [];
+    foreach($result as $ligne) {
+        $res[] = $ligne['nom_aliment']; 
+    }
+    echo json_encode($res); 
+
+   /*  $result = $connect->query('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
+    $liste = array();
+    $r = mysql_query($requete) or die (mysql_error());
+    while($resultat = mysql_fetch_assoc($r))
+    {$liste[]=htmlentities($resultat['nom_aliment']);} */
 
 
-
-
-
-
-
-
+    /* $request = $connect -> prepare('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
+    $request -> execute();
+    $table_nom = array (); 
+    while ($data = $request -> fetch())
+    $table_nom[]=$data['nom_aliment'];
+    
+    $number = $request -> rowcount();
+    for ($i =0; $i<$number; $i++)
+    {
+        echo $table_nom[$i];
+    
+    }
+ */
 
 ?>
