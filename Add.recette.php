67ad99f741d361d5ad7a3e06abba6431b630d7eb
Victor Janneteau <victor.janneteau@laposte.net>
2020-04-12 20:42:05 +0200

    CC

--- Add.recette.php
+++ Add.recette.php
@@ -1,33 +1,13 @@
 <?php include("Connect.php"); ?>
+
+
 <?php
-  
+    
     $result = $connect->query('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
-    $result = $result->fetchAll(PDO::FETCH_ASSOC);
+    $result = $result->fetchAll(PDO::FETCH_ASSOC); //transformation des résultats de la requete sous forme d'un tableau
     $res = [];
     foreach($result as $ligne) {
         $res[] = $ligne['nom_aliment']; 
     }
     echo json_encode($res); 
-
-   /*  $result = $connect->query('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
-    $liste = array();
-    $r = mysql_query($requete) or die (mysql_error());
-    while($resultat = mysql_fetch_assoc($r))
-    {$liste[]=htmlentities($resultat['nom_aliment']);} */
-
-
-    /* $request = $connect -> prepare('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
-    $request -> execute();
-    $table_nom = array (); 
-    while ($data = $request -> fetch())
-    $table_nom[]=$data['nom_aliment'];
-    
-    $number = $request -> rowcount();
-    for ($i =0; $i<$number; $i++)
-    {
-        echo $table_nom[$i];
-    
-    }
- */
-
 ?>
--- nv_recette.php
+++ nv_recette.php
@@ -1,34 +1,27 @@
+<!DOCTYPE html>
 <html>
   <?php include("entete.php"); ?>
   <?php include("police.php"); ?>
   <?php include("Connect.php"); ?>
   
-  
-  
-  
-<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
 
-
-<!DOCTYPE html>
-<html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>     
+
 <body>
 
-<!--Make sure the form has the autocomplete function switched off:-->
-<form autocomplete="off" action="Add.recette.php">
-  <div class="autocomplete" style="width:300px;">
-    <input id="myInput" type="text" name="myCountry" placeholder="Country">
-  </div>
-  <input type="submit">
-</form>
+  <form autocomplete="off" action="Add.recette.php">
+    <div class="autocomplete" style="width:300px;">
+      <input id="myInput" type="text" name="myAliment" placeholder="Aliment">
+    </div>
+    <input type="submit">
+  </form>
 
 <script>
+//the autocomplete function takes two arguments,the text field element and an array of possible autocompleted values
 function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
   var currentFocus;
   /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function(e) {
@@ -66,6 +59,7 @@ function autocomplete(inp, arr) {
         }
       }
   });
+
   /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
@@ -91,25 +85,25 @@ function autocomplete(inp, arr) {
         }
       }
   });
+   
+  // function to classify an item as "active":
   function addActive(x) {
-    /*a function to classify an item as "active":*/
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
+  
+  //function to remove the "active" class from all autocomplete items
   function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
+  
+  //close all autocomplete lists in the document, except the one passed as an argument 
+  function closeAllLists(elmnt) { 
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
@@ -117,28 +111,26 @@ function autocomplete(inp, arr) {
       }
     }
   }
-  /*execute a function when someone clicks in the document:*/
+  
+  //execute a function when someone clicks in the document 
   document.addEventListener("click", function (e) {
       closeAllLists(e.target);
   });
 }
 
-/*An array containing all the country names in the world:*/
-//var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+  //initiate the autocomplete function on the "myInput" element, and pass along the array as possible autocomplete values
+  var countries = $(document).ready(function () {
+    let countries = null;
+    $.get('Add.recette.php')
+        .done(function (data) {
+            countries = JSON.parse(data);
+            autocomplete(document.getElementById("myInput"), countries); 
+        });
 
-var countries = $(document).ready(function() {
-      let countries = null;
-      console.log(countries);
-      $.get('Add.recette.php')
-      .done(function(data){
-            countries = data;
-            console.log(countries);
-      });
-     
- })
+  })
 
-/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-autocomplete(document.getElementById("myInput"), countries);
+
+
 </script>
 
 </body>
