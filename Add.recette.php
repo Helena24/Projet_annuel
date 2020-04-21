230bbf93f68735185d59d8361696aa7cb093e6ad
Helena24 <Helena.adi@live.fr>
2020-04-21 17:19:42 +0200

    vvisu recette + auto complete sur aliment et ingredient

--- Add.recette.php
+++ Add.recette.php
@@ -3,11 +3,13 @@
 
 <?php
     
-    $result = $connect->query('SELECT nom_aliment FROM ALIMENTS ORDER BY NOM_ALIMENT');
+    $result = $connect->query('SELECT nom_aliment, nom_ingredient FROM ALIMENTS, INGREDIENTS ORDER BY NOM_ALIMENT, NOM_INGREDIENT');
     $result = $result->fetchAll(PDO::FETCH_ASSOC); //transformation des résultats de la requete sous forme d'un tableau
     $res = [];
+    $resu = [];
     foreach($result as $ligne) {
         $res[] = $ligne['nom_aliment']; 
+        $res[] = $ligne['nom_ingredient']; 
     }
     echo json_encode($res); 
 ?>
