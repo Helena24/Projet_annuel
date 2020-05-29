1312cdf47080ed9c94bacc9c47bcdb25ae680f32
Helena <Helena.adi@live.fr>
2020-05-29 15:50:03 +0200

    comm

--- suppr_aliment.php
+++ suppr_aliment.php
@@ -1,7 +1,20 @@
-<!DOCTYPE html>
+<!-- 
+    Page qui affiche la liste des
+    aliments avec le bouton supprimer
+    Cette page n'est disponible que pour
+    l'administrateur et la requete se fait 
+    dans la page Delete.aliment.php --->
+
+	<!DOCTYPE html>
 <html>
-<?php include("Functions.php");?>
-<?php include("Connect.php"); ?>
+<?php 
+    // Appel de la fonction pour afficher l'entête selon l'utilisateur
+    include("Functions.php"); 
+?>
+<?php 
+    // Appel de la page qui permet de connecter à la base de données
+    include("Connect.php"); 
+?>
 <head>
 <title>Suppression Aliment</title>
 </head> 
@@ -9,6 +22,7 @@
 
 <table class="content-table">
         <tr>
+            <!-- Nom entete de tableau -->
             <th>Nom</th>
             <th>Catégorie</th>
             <th>Calories</th>
@@ -19,18 +33,21 @@
         </tr>
 
 <?php
+// REcuperation de tous les aliments
 $reponse = $connect->query('SELECT * FROM ALIMENTS');
 // On affiche chaque entrée une à une
 while ($donnees = $reponse->fetch())
 {
 ?>
     <tr>
+        <!-- Affichage des aliments -->
         <td><?php echo $donnees['NOM_ALIMENT']; ?></td>
         <td><?php echo $donnees['CATEGORIE_ALIMENT']; ?></td>
         <td><?php echo $donnees['CALORIE_ALIMENT']; ?></td>
         <td><?php echo $donnees['PROTEINE_ALIMENT']; ?></td>
         <td><?php echo $donnees['GLUCIDE_ALIMENT']; ?></td>
         <td><?php echo $donnees['LIPIDE_ALIMENT']; ?></td>
+        <!-- Bouton pour supprimer -->
         <td><a href="Delete.aliment.php?idAliment=<?= $donnees['ID_ALIMENT']?>">Supprimer</a>
         </td>
     </tr>
--- suppr_ingredient.php
+++ suppr_ingredient.php
@@ -1,7 +1,20 @@
-<!DOCTYPE html>
+<!-- 
+    Page qui affiche la liste des
+    ingrédient avec le bouton supprimer
+    Cette page n'est disponible que pour
+    l'administrateur et la requete se fait 
+    dans la page Delete.ingredient.php --->
+
+	<!DOCTYPE html>
 <html>
-<?php include("Functions.php"); ?>
-<?php include("Connect.php"); ?>
+<?php 
+    // Appel de la fonction pour afficher l'entête selon l'utilisateur
+    include("Functions.php"); 
+?>
+<?php 
+    // Appel de la page qui permet de connecter à la base de données
+    include("Connect.php"); 
+?>
 <head>
 <title>Ingrédients</title>
 </head> 
@@ -14,13 +27,16 @@
         </tr>
 
 <?php
+// Recuperationd de tous les ingrédients
 $reponse = $connect->query('SELECT * FROM INGREDIENTS');
 // On affiche chaque entrée une à une
 while ($donnees = $reponse->fetch())
 {
 ?>
     <tr>
+        <!-- Affichage des ingrédient --> 
         <td><?php echo $donnees['NOM_INGREDIENT']; ?></td>
+        <!-- Bouton pour supprimer -->
         <td><a href="Delete.ingredient.php?idIngredient=<?= $donnees['ID_INGREDIENT']?>">Supprimer</a></td>
     </tr>
 <?php
